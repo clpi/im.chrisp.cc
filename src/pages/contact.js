9acbd5ba6097363bbd4dac12902fb38cd34f21b2
Chris P <chrisp@localhost.localdomain>
2020-01-31 01:51:22 -0800

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,6 +9,21 @@ const Contact = (props) => (
     <SEO title="contact" />
     <h1>contact</h1>
     <p>if for some reason you need to contact me right now, email me at im@chrisp.cc</p>
+    <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
   </Layout>
 )
 
